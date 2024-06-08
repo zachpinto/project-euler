6c07c8737cf221994545bd315bc473a83226d582
Zachary TW Pinto <zachwalker.pinto@gmail.com>
2024-06-08 10:48:46 -0400

    problem 58

---
+++ 51-60/problem_58.py
@@ -0,0 +1,54 @@
+"""
+Problem 58
+
+Spiral primes
+
+Starting with 1 and spiralling anticlockwise in the following way, a square spiral with side length 7 is formed.
+
+37 36 35 34 33 32 31
+38 17 16 15 14 13 30
+39 18  5  4  3 12 29
+40 19  6  1  2 11 28
+41 20  7  8  9 10 27
+42 21 22 23 24 25 26
+43 44 45 46 47 48 49
+
+It is interesting to note that the odd squares lie along the bottom right diagonal, but what is more interesting is
+that 8 out of the 13 numbers lying along both diagonals are prime; that is, a ratio of 8/13 ≈ 62%.
+
+If one complete new layer is wrapped around the spiral above, a square spiral with side length 9 will be formed.
+ If this process is continued, what is the side length of the square spiral for which the ratio of primes along
+ both diagonals first falls below 10%?
+
+"""
+
+import sympy
+
+
+def user_input():
+    limit = float(input("Please choose a limit: "))
+    return limit
+
+
+def spiral_primes(limit):
+    prime_count = 0
+    total_count = 1
+    side_length = 1
+    while True:
+        side_length += 2
+        for i in range(4):
+            num = side_length ** 2 - i * (side_length - 1)
+            if sympy.isprime(num):
+                prime_count += 1
+        total_count += 4
+        if prime_count / total_count < limit:
+            return side_length
+
+
+def main():
+    limit = user_input()
+    result = spiral_primes(limit)
+    print(result)
+
+
+main()
